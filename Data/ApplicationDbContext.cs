a4935fa901e03fd843f0ea457ccb93c821826dd8
muhammed bilal köseoğlu <55670982+mbk25@users.noreply.github.com>
2024-05-10 16:50:18 +0300

    Data

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Restorant.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restorant.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Kategori> Kategoriler { get; set; }
+
+        public DbSet<Menu> Menuler { get; set; }
+
+        //public DbSet<Tatlı> Tatlılar { get; set; }
+    }
+}
